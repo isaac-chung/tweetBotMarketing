887ec71d8a8adacf412a659a4a247b04c9ba6d5e
Isaac Chung <isaac.chung@clarifai.com>
2023-08-23 00:34:10 +0300

    randomizing prompts

--- constants.py
+++ constants.py
@@ -4,9 +4,12 @@ APP_ID = 'Llama-2'
 MODEL_ID = 'llama2-70b-chat'
 MODEL_VERSION_ID = '6c27e86364ba461d98de95cddc559cb3'
 
-PROMPT_2 = "Prepare a tweet to promote Clarifai's hackathon. Only output the raw text of the example. Do not include instructions. Include the hashtag '#Clarifai'. Limit the entire example to 220 characters. The registration link is https://lablab.ai/event/llama-2-hackathon-with-clarifai. Include it as well."
-PROMPT = "Prepare a tweet to promote Clarifai's LLM finetuning capabilities. Only output the raw text of the example. Do not include instructions. Include the hashtag '#Clarifai'. Include the relevant link to a blogpost: 'https://www.clarifai.com/blog/fine-tuning-gpt-neo-for-text-classification'. Limit the entire example to 220 characters."
-
+PROMPTS = [
+    "Prepare a tweet to promote Clarifai's hackathon. Only output the raw text of the example. Do not include instructions. Include the hashtag '#Clarifai'. Limit the entire example to 220 characters. The registration link is https://lablab.ai/event/llama-2-hackathon-with-clarifai. Include it as well.",
+    "Prepare a tweet to promote Clarifai's LLM finetuning capabilities. Only output the raw text of the example. Do not include instructions. Include the hashtag '#Clarifai'. Include the relevant link to a blogpost: 'https://www.clarifai.com/blog/fine-tuning-gpt-neo-for-text-classification'. Limit the entire example to 240 characters.",
+    "Prepare a tweet to promote Clarifai's LLM finetuning capabilities. Only output the raw text of the example. Do not include instructions. Include the hashtag '#Clarifai'. Include the relevant link to a video tutorial: 'https://www.youtube.com/watch?v=Ycl7xVA2wHk'. Limit the entire example to 240 characters.",
+    "Prepare a tweet to promote Clarifai's vector search capabilities. Only output the raw text of the example. Do not include instructions. Include the hashtag '#Clarifai'. Limit the entire example to 260 characters."
+]
 
 ## For building the HTML
 TWITTER_USER_ID='i48456'
