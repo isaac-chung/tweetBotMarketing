99abd943aa0c89fea4ab3c8ad70a3b3978e998cc
Isaac Chung <isaac.chung@clarifai.com>
2023-08-24 09:10:20 +0300

    remove hack

--- constants.py
+++ constants.py
@@ -8,7 +8,6 @@ MODEL_VERSION_ID = '6c27e86364ba461d98de95cddc559cb3'
 
 PROMPT_TEMPLATE = Template("Prepare a tweet to promote Clarifai's $highlight. Only output the raw text of the example. Do not include instructions. Include the hashtags '$hashtags'. Limit the entire example to 260 characters. Include the relevant link $optional `$link`. Limit the entire example to 260 characters including the link.")
 PROMOTION_HASHTAG_LINK_TUPLES = [
-    ("hackathon", "#llms", "to register", "https://lablab.ai/event/llama-2-hackathon-with-clarifai"),
     ("LLM finetuning capabilities", "#llms", "to a blogpost", "https://www.clarifai.com/blog/fine-tuning-gpt-neo-for-text-classification"),
     ("LLM finetuning capabilities", "#llms", "to a video tutorial", "https://www.youtube.com/watch?v=Ycl7xVA2wHk"),
     ("vector search capabilities", "#llms", "", "https://www.clarifai.com/use-cases/visual-search"),
