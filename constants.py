9303569e3faaad53f5d3a1e81a2ae8f770c2db1f
Isaac Chung <isaac.chung@clarifai.com>
2023-08-24 12:42:57 +0300

    add more prompts

--- constants.py
+++ constants.py
@@ -6,20 +6,31 @@ APP_ID = 'Llama-2'
 MODEL_ID = 'llama2-70b-chat'
 MODEL_VERSION_ID = '6c27e86364ba461d98de95cddc559cb3'
 
-PROMPT_TEMPLATE = Template("Prepare an example tweet to promote Clarifai's $highlight. Only output the raw text of the example. Do not include instructions. Include the hashtags '$hashtags'. Limit the entire example to 260 characters. Include the relevant link $optional `$link`. Limit the entire example to 260 characters including the link.")
+PROMPT_TEMPLATE = Template("Prepare a tweet to promote $highlight. Only output the raw text of the tweet without any instructions or any preamble. Include the hashtags '$hashtags'. Include the relevant link $optional `$link`. Limit the tweet to 260 characters including the link.")
 PROMOTION_HASHTAG_LINK_TUPLES = [
-    ("hackathon", "#llms", "to register", "https://lablab.ai/event/llama-2-hackathon-with-clarifai"),
-    ("LLM finetuning capabilities", "#llms", "to a blogpost", "https://www.clarifai.com/blog/fine-tuning-gpt-neo-for-text-classification"),
-    ("LLM finetuning capabilities", "#llms", "to a video tutorial", "https://www.youtube.com/watch?v=Ycl7xVA2wHk"),
-    ("vector search capabilities", "#llms", "", "https://www.clarifai.com/use-cases/visual-search"),
-    ("generative AI capabilities", "#generativeAI", "", "https://www.clarifai.com/products/generative-ai"),
-    ("content moderation capabilities", "#llms", "", "https://www.clarifai.com/solutions/content-moderation"),
-    ("DAM capabilities", "#Clarifai", "", "https://www.clarifai.com/solutions/digital-asset-management"),
-    ("predictive maintenance capabilities", "#Clarifai", "", "https://www.clarifai.com/use-cases/predictive-maintenance"),
-    ("Edge AI capabilities", "#Clarifai", "", "https://www.clarifai.com/use-cases/isr-edge-ai"),
-    ("open positions", "#hiring", "", "https://www.clarifai.com/company/careers"),
-    ("LLM prompt capabilities", "#llms", "", "https://docs.clarifai.com/api-guide/predict/llms/#via-raw-text"),
-    ("serverless inference capabilities", "#llms", "", "https://docs.clarifai.com/api-guide/predict/llms/#via-raw-text")
+    ("Clarifai's hackathon", "#llms", "to register", "https://lablab.ai/event/llama-2-hackathon-with-clarifai"),
+    ("Clarifai's LLM finetuning capabilities", "#llms", "to a blogpost", "https://www.clarifai.com/blog/fine-tuning-gpt-neo-for-text-classification"),
+    ("Clarifai's LLM finetuning capabilities", "#llms", "to a video tutorial", "https://www.youtube.com/watch?v=Ycl7xVA2wHk"),
+    ("Clarifai's vector search capabilities", "#llms", "", "https://www.clarifai.com/use-cases/visual-search"),
+    ("Clarifai's generative AI capabilities", "#generativeAI", "", "https://www.clarifai.com/products/generative-ai"),
+    ("Clarifai's content moderation capabilities", "#llms", "", "https://www.clarifai.com/solutions/content-moderation"),
+    ("Clarifai's DAM capabilities", "#Clarifai", "", "https://www.clarifai.com/solutions/digital-asset-management"),
+    ("Clarifai's predictive maintenance capabilities", "#Clarifai", "", "https://www.clarifai.com/use-cases/predictive-maintenance"),
+    ("Clarifai's Edge AI capabilities", "#Clarifai", "", "https://www.clarifai.com/use-cases/isr-edge-ai"),
+    ("Clarifai's open positions", "#hiring", "", "https://www.clarifai.com/company/careers"),
+    ("Clarifai's LLM prompt capabilities", "#llms", "", "https://docs.clarifai.com/api-guide/predict/llms/#via-raw-text"),
+    ("Clarifai's serverless inference capabilities", "#llms", "", "https://docs.clarifai.com/api-guide/predict/llms/#via-raw-text"),
+    ("a Clarifai customer Acquia DAM", "#llms", "", "https://www.clarifai.com/customers/acquia-dam"),
+    ("a Clarifai customer that is a business retailer", "#llms", "", "https://www.clarifai.com/customers/business-retailer"),
+    ("a Clarifai customer Foap", "#llms", "", "https://www.clarifai.com/customers/foap"),
+    ("a Clarifai customer that is a DAM company", "#llms", "", "https://www.clarifai.com/customers/dam-company"),
+    ("a Clarifai customer that is a leading online travel booking site", "#llms", "", "https://www.clarifai.com/customers/online-travel-agency"),
+    ("a Clarifai customer that is an international mobile app company", "#llms", "", "https://www.clarifai.com/customers/mobile-phone-app"),
+    ("a Clarifai customer OpenTable", "#llms", "", "https://www.clarifai.com/customers/opentable"),
+    ("a Clarifai customer 9GAG", "#llms", "", "https://www.clarifai.com/customers/9gag"),
+    ("a Clarifai customer that is a Fortune 100 retailer", "#llms", "", "https://www.clarifai.com/customers/home-improvement-retailer"),
+    ("Clarifai's hosted AI foundation models", "#llms", "", "https://www.clarifai.com/blog/choosing-best-foundation-model-for-your-use-case"),
+    ("Clarifai's bulk labeling capabilities", "#AI #labeling", "", "https://www.clarifai.com/blog/bulk-labeling-the-fast-and-easy-way")
 ]
 
 PROMPTS = [
