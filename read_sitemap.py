fa6a263359998bcf6a14c9c332fb0d1875bf2bf9
Isaac Chung <chungisaac1217@gmail.com>
2023-10-11 14:53:18 +0300

    refactor

--- read_sitemap.py
+++ read_sitemap.py
@@ -1,5 +1,4 @@
 import os
-import tweepy
 import requests
 import pandas as pd
 import random
@@ -7,7 +6,7 @@ import xmltodict
 
 from constants import (PROMPT_TEMPLATE, YEAR_ONWARDS,
                        SITEMAP_URL, LASTMOD, SYSTEM_PROMPTS)
-from tweet import build_tweet_url
+from tweet import build_tweet_url, post_tweet
 from llm import ClarifaiPrompter
 from upload import ClarifaiUploader
 
@@ -16,8 +15,9 @@ consumer_secret = os.environ.get("CONSUMER_SECRET")
 access_token = os.environ.get("ACCESS_TOKEN")
 access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
 
-def main():
 
+def read_from_sitemap(year_onwards=YEAR_ONWARDS) -> str:
+    """return a random URL from the sitemap after filtering for year of lastmod."""
     res = requests.get(SITEMAP_URL)
     raw = xmltodict.parse(res.text)
     data = [[r["loc"], r[LASTMOD]] for r in raw["urlset"]["url"]]
@@ -26,9 +26,13 @@ def main():
 
     df = df[df["links"].str.contains("blog")]
     df[LASTMOD] = pd.to_datetime(df[LASTMOD])
-    df = df[df[LASTMOD]>f'{YEAR_ONWARDS}-01-01']
+    df = df[df[LASTMOD]>f'{year_onwards}-01-01']
 
-    chosen_url = random.choice(df['links'].tolist())
+    return random.choice(df['links'].tolist())
+
+
+def main():
+    chosen_url = read_from_sitemap()
     print(f"{chosen_url=}")
 
     topic = chosen_url.split("/")[-1].replace('-',' ')
@@ -47,15 +51,7 @@ def main():
             break
     print(tweet)
 
-    client = tweepy.Client(
-        consumer_key=consumer_key, consumer_secret=consumer_secret,
-        access_token=access_token, access_token_secret=access_token_secret
-    )
-
-    response = client.create_tweet(
-        text=tweet
-    )
-    tweet_id = response.data['id']
+    tweet_id = post_tweet(tweet)
     print(f"Tweet ID: {tweet_id}")
 
     tweet_url = build_tweet_url(tweet_id)
@@ -64,8 +60,8 @@ def main():
     ## https://github.com/xacnio/tweetcapture/blob/main/tweetcapture/examples/tweet_screenshot.py
     from tweetcapture import TweetCapture
     import asyncio
-    tweet = TweetCapture()
-    asyncio.run(tweet.screenshot(tweet_url, image_path, mode=3, night_mode=0))
+    tc = TweetCapture()
+    asyncio.run(tc.screenshot(tweet_url, image_path, mode=3, night_mode=0))
 
     ClarifaiUploader().upload(image_file_location=image_path)
 
